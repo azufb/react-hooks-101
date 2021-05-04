5d62a547b02ab2b09de80e3bc5c426f97d6b0e25
azufb <curiousazunas@gmail.com>
2021-05-04 18:13:46 +0900

    hooks導入

--- src/App.js
+++ src/App.js
@@ -1,9 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
+
+  const [count, setCount] = useState(0); // 配列を返す。2つの要素を返す。変数と関数(setほにゃらら)。
+
+  // 足し算
+  function increment() {
+    setCount(count+1);
+  }
+
+  function increment2() {
+    setCount(previousCount => previousCount+1)
+  }
+
+  // 引き算
+  function decrement() {
+    setCount(count-1);
+  }
+
+  // 2倍
+  function double() {
+    setCount(count*2);
+  }
+
+  // resetで、初期値に戻す
+  function reset() {
+    setCount(0);
+  }
+
+  // 3の倍数のときだけで3で割る
+  function divide3() {
+    if (count%3 === 0) {
+      setCount(count/3);
+    }
+  }
+
   return (
     <div>
-      This is a template for React app!
+      <div>count：{count}</div>
+      <button onClick={increment}>+1</button>
+      <button onClick={decrement}>-1</button>
+      <button onClick={double}>×2</button>
+      <div>
+        <button onClick={increment2}>+1</button>
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+      </div>
+      <div>
+        <button onClick={divide3}>3の倍数のときだけ3で割る</button>
+      </div>
     </div>
   );
 }
